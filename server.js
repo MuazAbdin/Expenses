c81ee4e5300c2bee9006faf19a74b99283ed0090
Muaz Abdin <muathabd25@gmail.com>
2024-01-16 20:04:59 +0200

    some fixes

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import morgan from "morgan";
 import express from "express";
+const app = express();
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import morgan from "morgan";
 import { connect } from "mongoose";
 import api from "./server/routes/api.js";
 import populateDB from "./populate.js";
 
-const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(morgan("dev"));
+app.use("/public", express.static(path.resolve(__dirname, "./client")));
+app.use("/public", express.static(path.resolve(__dirname, "./node_modules")));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/expenses", api);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  const statusCode = err.statusCode || 500;
+  const msg = err.message || "something went wrong, try again later";
+  res.status(statusCode).send({ msg });
+});
 
 const PORT = process.env.PORT || 3000;
 try {
@@ -22,7 +37,7 @@ try {
   // console.log("DB is populated successfully ...");
   app.listen(PORT, () => {
     console.log(
-      `server running on PORT ${PORT} ... <http://localhost:${PORT}/>`
+      `server running on PORT ${PORT} ... <http://localhost:${PORT}/public>`
     );
   });
 } catch (error) {
